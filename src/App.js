2dd8b3527c0ab2da82111653de2fac58e7f4a497
zrama19 <ramazanovzarif@gmail.com>
2022-02-03 16:29:47 -0500

    coding interview react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+const url = `https://randomuser.me/api?page=1`;
 function App() {
+  const [userData, setUserData] = useState([]);
+  const getMoreUsers = () => {
+    axios
+      .get(url)
+      .then((response) => {
+        setUserData([...userData, ...response.data.results]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getMoreUsers();
+  }, []);
+  console.log(userData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={getMoreUsers}>Generate More </button>
+      <div>
+        {userData.map((data, idx) => {
+          return (
+            <div key={idx}>
+              <p>{data.gender}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
